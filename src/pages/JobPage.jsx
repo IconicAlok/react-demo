2aae280e5fb208af568dd3f4923764c4286aae03
alok-kuri <alokkuri98@gmail.com>
2025-10-08 10:35:18 +0600

    React development

---
+++ src/pages/JobPage.jsx
@@ -0,0 +1,28 @@
+import { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+import Spinner from '../component/Spinner';
+
+const JobPage = () => {
+  const [job, setJob] = useState(null);
+  const { id } = useParams;
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchJob = async () => {
+      try {
+        const res = await fetch(`/api/jobs/${id}`);
+        const data = await res.json();
+        setJob(data);
+      } catch (error) {
+        console.log('Error fatching data', error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchJob();
+  }, []);
+  return loading ? <Spinner /> : <h1>{job.title}</h1>;
+};
+
+export default JobPage;
